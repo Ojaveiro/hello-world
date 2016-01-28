38841f37bad2e85a07858a149dcc8fbb1461dc10
EduardoCortes <Eduardo.lima@fcamara.com.br>
2016-01-28 13:18:06 -0200

    commit inicial.

---
+++ comercial-web/src/main/java/br/com/ideatech/comercial/Pessoa.java
@@ -0,0 +1,28 @@
+package br.com.ideatech.comercial;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	
+	public void pegarNomeDaPessoa(String nome){
+		this.nome = "O nome é " + nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
