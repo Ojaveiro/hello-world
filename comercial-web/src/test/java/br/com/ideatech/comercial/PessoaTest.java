cc5c0fbce4e9a51e29796dfd4aa792e25f8c753a
eduardocortes <eduardo.lima@fcamara.com.br>
2016-01-29 15:42:54 -0200

    junit

---
+++ comercial-web/src/test/java/br/com/ideatech/comercial/PessoaTest.java
@@ -0,0 +1,14 @@
+package br.com.ideatech.comercial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PessoaTest {
+
+	@Test
+	public void testPegarNomeDaPessoa() {
+		fail("Not yet implemented");
+	}
+
+}
